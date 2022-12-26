fe44cbb438a9bd739d73c6ae6be0b3053daa18fc
MaddMonkey81 <47820089+MaddMonkey81@users.noreply.github.com>
2022-12-26 17:00:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/', (req, res) => {
       
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(8080, () => {
+    console.log('Server listening on port 8080');
     console.log('Press Ctrl+C to quit.');
 });
